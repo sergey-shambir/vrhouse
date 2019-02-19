9c7c47e83fc0b9f7386e7756369675e0f9b3dce5
Sergey Shambir <sshambir@yandex.ru>
2019-02-19 08:57:52 +0300

    Дальнейшие доработки каркаса frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,85 @@
-import _ from 'lodash';
+import WebVRPolyfill from 'webvr-polyfill';
+import * as THREE from 'three';
 
-function component() {
-    let element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+if (!navigator.getVRDisplays) {
+    console.warn("no native WebVR support: starting polyfill");
+    new WebVRPolyfill();
 }
 
-document.body.appendChild(component());
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const orbitControls = new THREE.OrbitControls(camera);
+
+// Store the position of the VR HMD in a dummy camera.
+const fakeCamera = new THREE.Object3D();
+const vrControls = new THREE.VRControls(fakeCamera);
+const vrEffect = new THREE.VREffect(renderer, () => { });
+
+const scene = createScene(orbitControls);
+/**
+ * @param {THREE.OrbitControls} orbitControls
+ */
+function createScene(orbitControls) {
+    const scene = new THREE.Scene();
+    scene.add(new THREE.PointLight());
+
+    const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshLambertMaterial({
+            color: 'green'
+        })
+    );
+    cube.position.set(-1, -2, -5);
+    scene.add(cube);
+    orbitControls.target = cube.position;
+
+    for (var i = 0; i < 10; i++) {
+        cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshLambertMaterial()
+        );
+        cube.position.set(
+            (Math.random() - 0.5) * 20,
+            (Math.random() - 0.5) * 20,
+            (Math.random() - 0.5) * 20
+        );
+        scene.add(cube);
+    }
+}
+
+function render() {
+    requestAnimationFrame(render);
+
+    orbitControls.update();
+    vrControls.update();
+
+    // Temporarily save the orbited camera position
+    var orbitPos = camera.position.clone();
+
+    // Apply the VR HMD camera position and rotation
+    // on top of the orbited camera.
+    var rotatedPosition = fakeCamera.position.applyQuaternion(
+        camera.quaternion);
+    camera.position.add(rotatedPosition);
+    camera.quaternion.multiply(fakeCamera.quaternion);
+
+    vrEffect.render(scene, camera);
+
+    // Restore the orbit position, so that the OrbitControls can
+    // pickup where it left off.
+    camera.position.copy(orbitPos);
+}
+
+function createVRView() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    return renderer.domElement;
+}
+
+window.addEventListener('resize', function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    vrEffect.setSize(window.innerWidth, window.innerHeight);
+}, false);
+
+document.body.appendChild(createVRView());
+render();
--- server/app/index.js
+++ server/app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-const staticDir = path.resolve(path.join(__dirname, '..', 'public'));
+const staticDir = path.resolve(path.join(__dirname, '..', '..', 'frontend', 'dist'));
 
 const app = express();
 
